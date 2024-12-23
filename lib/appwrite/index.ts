44ccee5148413059b2716aae502e2b809b277b38
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2024-12-23 15:45:18 +0300

    Appwrite (#2)
    * add assets
    
    * add shadcn
    
    * add shadcn button, form, input, label
    
    * add untiltiy function to to merge classname
    
    * add layout to sign-in and sign-up
    
    * add sign in page
    
    * add sign up page
    
    * add auth form component
    
    * add appwrite configuration

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,40 @@
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "./config";
+import { cookies } from "next/headers";
+
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+  const session = (await cookies()).get("appwrite-session");
+  if (!session || !session.value) throw new Error("Ne session");
+  client.setSession(session.value);
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+export const createAdminClient = async () => {
+  const admin = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+  return {
+    get account() {
+      return new Account(admin);
+    },
+    get databases() {
+      return new Databases(admin);
+    },
+    get storage() {
+      return new Storage(admin);
+    },
+    get avatars() {
+      return new Avatars(admin);
+    },
+  };
+};
