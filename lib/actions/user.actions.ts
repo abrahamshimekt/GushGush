af90c07ee28e1e3196e665d4c8e050e07c3a5550
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-24 15:43:01 +0300

    add get user action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserbyEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -47,8 +48,7 @@ export const createAccount = async ({
       {
         fullName,
         email,
-        avatar:
-          "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+        avatar: avatarPlaceholderUrl,
         accountId,
       }
     );
@@ -77,3 +77,17 @@ export const verifySecret = async ({
     handleError(error, "Failed to verify OTP");
   }
 };
+
+
+export const getCurrentUser = async()=>{
+  const {databases,account} = await createSessionClient();
+  const result = await account.get();
+  const user = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal('accountId',result.$id)]
+
+  )
+  if(user.total <=0) return null;
+  return parseStringify(user.documents[0]);
+}
