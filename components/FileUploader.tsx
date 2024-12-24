ea83428e51e716043aeaff55eaabd2fe1daab429
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-24 18:55:27 +0300

    create file uploader component

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,7 +1,85 @@
-import React from "react";
+"use client";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+import Image from "next/image";
+import Thumbnail from "./Thumbnail";
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className: string;
+}
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const handleRemoveFile = (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    name: string
+  ) => {
+    e.preventDefault();
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== name));
+  };
+  return (
+    <div>
+      <div {...getRootProps()} className="cursor-pointer">
+        <input {...getInputProps()} />
+        <Button type="button" className={cn("uploader-button", className)}>
+          <Image
+            src="/assets/icons/upload.svg"
+            alt="upload"
+            width={24}
+            height={24}
+          />
+          <p>Upload</p>
+        </Button>
+      </div>
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
 
-const FileUploader = () => {
-  return <div>FileUploader</div>;
+            return (
+              <li
+                key={`${file.name}-${index}`}
+                className="uploader-preview-item"
+              >
+                <div className="flex items-center gap-3">
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+                  <div className="preview-item-name">
+                    {file.name}
+                    <Image
+                      src="/assets/icons/file-loader.gif"
+                      alt="loader"
+                      width={80}
+                      height={26}
+                    />
+                  </div>
+                </div>
+                <Image
+                  src="/assets/icons/remove.svg"
+                  width={24}
+                  height={24}
+                  alt="remove"
+                  onClick={(e) => handleRemoveFile(e, file.name)}
+                  className="cursor-pointer"
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default FileUploader;
