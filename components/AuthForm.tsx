5c6f8f3e6ef49ebd9b8b0044e74606dfea5d81ca
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-24 17:42:17 +0300

    add signin logic

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,7 +15,7 @@ import {
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import Link from "next/link";
-import { createAccount } from "@/lib/actions/user.actions";
+import { createAccount, signInUser } from "@/lib/actions/user.actions";
 import OTPModal from "./OTPModal";
 type FormType = "sign-in" | "sign-up";
 const authForm = (type: FormType) => {
@@ -42,10 +42,13 @@ const AuthForm = ({ type }: { type: FormType }) => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const user = await createAccount({
-        fullName: values.fullName || "",
-        email: values.email,
-      });
+      const user =
+        type === "sign-up"
+          ? await createAccount({
+              fullName: values.fullName || "",
+              email: values.email,
+            })
+          : await signInUser({ email: values.email });
       setAccountId(user.accountId);
     } catch (error) {
       setErrorMessage("Failed to create account. please try again");
