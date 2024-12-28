eb5f23cac32a4f83080b49f9f391695e5b5cfde3
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-28 18:31:58 +0300

    add delete file action

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -138,3 +138,23 @@ export const updateFileUsers = async ({
   revalidatePath(path);
   return parseStringify(updatedFile);
 };
+
+export const deleteFile = async ({
+  fileId,
+  bucketFileId,
+  path,
+}: DeleteFileProps) => {
+  const { databases, storage } = await createAdminClient();
+  try {
+    const deletedFile = await databases.deleteDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      fileId
+    );
+    if (deletedFile) {
+      await storage.deleteFile(appwriteConfig.bucketId, bucketFileId);
+    }
+    revalidatePath(path);
+    return parseStringify({ status: "success" });
+  } catch (error) {}
+};
