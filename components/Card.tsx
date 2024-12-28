ae2f578815fd0c7ad8452682d94d5ebec6803b53
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2024-12-28 13:58:54 +0300

    Action dropdown (#14)
    * add assets
    
    * add shadcn
    
    * add shadcn button, form, input, label
    
    * add untiltiy function to to merge classname
    
    * add layout to sign-in and sign-up
    
    * add sign in page
    
    * add sign up page
    
    * add auth form component
    
    * add appwrite configuration
    
    * remove next default tsx
    
    * add user actions
    
    * add OTP
    
    * add constants
    
    * add get user action
    
    * add remote patterns
    
    * add sidebar
    
    * add header
    
    * add root layout
    
    * move page
    
    * add moved page
    
    * create file uploader
    
    * create mobile nav
    
    * create search
    
    * add shadcn sheet and separator
    
    * pass current user to sidebar and mobile nav
    
    * add shadcn separator
    
    * add shadcn mobile nav
    
    * add mobile navigation
    
    * add signout functionality
    
    * fix logout server action when using server component
    
    * add signin logic
    
    * add sign in user server action
    
    * add react dropezone
    
    * create file uploader component
    
    * add file uploader thumbnail
    
    * add utility functions
    
    * pass props
    
    * add shadcn toast
    
    * add size limit for server actions
    
    * add props
    
    * upload files to appwrite(BAAS)
    
    * add props
    
    * add additional props
    
    * add shadcn toast
    
    * add shadcn
    
    * fix error string
    
    * add file server actions
    
    * make handle error async
    
    * add types
    
    * add get files server action
    
    * fix file extension
    
    * use card component
    
    * create card component
    
    * create formatted date time component
    
    * add shadcn dropdown menu
    
    * create action drop component
    
    * use action drop down
    
    * add shadcn dialog
    
    * add shadcn menu to component ui

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Thumbnail from "./Thumbnail";
 import FormattedDateTime from "./FormattedDateTime";
 import { convertFileSize } from "@/lib/utils";
-
+import ActionDropdown from "./ActionDropdown";
 const Card = ({ file }: { file: Models.Document }) => {
   return (
     <Link href={file.url} target="_blank" className="file-card">
@@ -17,6 +17,7 @@ const Card = ({ file }: { file: Models.Document }) => {
           imageClassName="!size-11"
         />
         <div className="flex flex-col items-end justify-between">
+          <ActionDropdown file={file}/>
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
