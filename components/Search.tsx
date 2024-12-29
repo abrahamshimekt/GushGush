ab01e09ed00710152df6a4ded3107b86c75eb534
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2024-12-29 08:47:41 +0300

    Debounce (#21)
    * add share file
    
    * add delete file action
    
    * call delete user action
    
    * add filter by file types
    
    * add search
    
    * add debounce for the search

--- components/Search.tsx
+++ components/Search.tsx
@@ -7,7 +7,7 @@ import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
 import Thumbnail from "./Thumbnail";
 import FormattedDateTime from "./FormattedDateTime";
-
+import { useDebounce } from "use-debounce";
 const Search = () => {
   const [query, setQuery] = useState("");
   const searchParams = useSearchParams();
@@ -16,19 +16,20 @@ const Search = () => {
   const [open, setOpen] = useState(false);
   const router = useRouter();
   const path = usePathname();
+  const [debouncedQuery] = useDebounce(query, 300);
   useEffect(() => {
     const fetchFiles = async () => {
-      if (!query) {
+      if (debouncedQuery.length === 0) {
         setResults([]);
         setOpen(false);
         return router.push(path.replace(searchParams.toString(), ""));
       }
-      const files = await getFiles({ searchText: query });
+      const files = await getFiles({ searchText: debouncedQuery });
       setResults(files.documents);
       setOpen(true);
     };
     fetchFiles();
-  }, [query]);
+  }, [debouncedQuery]);
   useEffect(() => {
     if (!searchQuery) {
       setQuery("");
